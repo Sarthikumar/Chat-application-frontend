7b5c3a7010c043ebe863260d5ebd0600c6a24ad0
Your Name <you@example.com>
2019-12-17 00:14:22 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,62 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
+import { ChatModule } from './chat/chat.module';
+import {RouterModule,Routes} from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
+import { AppService } from './app.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SignupComponent } from './user/signup/signup.component';
+import { SharedModule } from './shared/shared.module';
+
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("Google-OAuth-Client-Id")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("Facebook-App-Id")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, //Browser Module comes by default in the app module.The app module is a Entry module for the application
+    UserModule,
+    ChatModule,
+    SharedModule,
+    SocialLoginModule,
+    HttpClientModule,
+    ToastModule.forRoot(),
+    BrowserAnimationsModule,
+
+    RouterModule.forRoot([
+      {path:'login',component:LoginComponent,pathMatch:'full'},
+    {path:'',redirectTo:'login',pathMatch:'full'}, // So as per this when we give localhost:4200/ then it goes in this path
+    //{path:'',component:SignupComponent,pathMatch:'full'}, // So as per this when we give localhost:4200/ then it goes in this path
+      {path:'*',component:SignupComponent},
+     {path:'**',component:LoginComponent}
+    ])
   ],
-  providers: [],
+  providers: [AppService,{
+    provide: AuthServiceConfig,
+    useFactory: provideConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user/login/login.component.ts
@@ -0,0 +1,110 @@
+import { Component, OnInit,ViewContainerRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { Cookie } from 'ng2-cookies/ng2-cookies';
+import { AppService } from './../../app.service';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+import { AuthService } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+
+import { SocialUser } from "angularx-social-login";
+
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  public email: any;
+  public password: any;
+
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+
+  constructor( public appService: AppService,
+    public router: Router,
+    private toastr: ToastsManager,private authService: AuthService,
+    vcr: ViewContainerRef) {
+      this.toastr.setRootViewContainerRef(vcr);
+     }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  } 
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+  public goToSignUp: any = () => {
+
+    this.router.navigate(['/sign-up']);
+
+  } // end goToSignUp
+
+  public signinFunction: any = () => {
+
+    if (!this.email) {
+      this.toastr.warning('enter email')
+
+
+    } else if (!this.password) {
+
+      this.toastr.warning('enter password')
+
+
+    } else {
+
+      let data = {
+        email: this.email,
+        password: this.password
+      }
+
+      this.appService.signinFunction(data)
+        .subscribe((apiResponse) => {
+
+          if (apiResponse.status === 200) {
+            console.log(apiResponse)
+
+             Cookie.set('authtoken', apiResponse.data.authToken);
+            
+             Cookie.set('receiverId', apiResponse.data.userDetails.userId);
+            
+             Cookie.set('receiverName', apiResponse.data.userDetails.firstName + ' ' + apiResponse.data.userDetails.lastName);
+           
+             this.appService.setUserInfoInLocalStorage(apiResponse.data.userDetails)
+            // alert(Cookie.get('receiverName'));
+               alert(this.appService.getUserInfoFromLocalstorage()["userId"]);
+            
+             this.router.navigate(['/chat']);
+
+          } else {
+
+            this.toastr.error(apiResponse.message)
+          
+
+          }
+
+        }, (err) => {
+          this.toastr.error('some error occured')
+
+        });
+
+    } // end condition
+
+  } // end signinFunction
+
+}
